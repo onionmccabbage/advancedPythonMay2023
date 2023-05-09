191824d5a5a1a117ec33bc939885ec1c1b047989
onionmccabbage <onionmccabbage@live.com>
2023-05-09 10:18:37 +0100

    basic shape class

---
+++ some_classes/class_a.py
@@ -0,0 +1,16 @@
+# Python Classes encapsulate functionality
+# Unlike other languages there is NO data-type-safety and NO public/private
+
+class Shape():
+    '''It is common practice to document your classes with a docstring
+       Every method of a class must take 'self' as an argument'''
+    def __init__(self, num_sides):
+        '''the __init__ method is similar to a constructor.
+           Runs every time we instantiate the class'''
+        self.sides = num_sides # here we assign a property to this class
+
+if __name__ == '__main__':
+    '''here we can exercise the code. E.G. make instances of the class'''
+    square = Shape(4) # the __init__ method will be called
+    triangle = Shape(3)
+    print(square.sides, triangle.sides)
