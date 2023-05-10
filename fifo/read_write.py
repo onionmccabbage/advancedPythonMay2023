622ecb1883d1bc16b4e0e1716031bc25f3001f79
onionmccabbage <onionmccabbage@live.com>
2023-05-10 10:38:50 +0100

    fifo text

--- fifo/read_write.py
+++ fifo/read_write.py
@@ -3,6 +3,7 @@
 
 # write text (defaults to text)
 fout = open('snip.txt', 'at') # 'a' will append, 'w' will (over)write, 'x' for exclusive access
+# NB print will automatically put a newline at the end of each output
 print('here is some content', file=fout ) # re-direct the print oputput to a text file
 # we have a file access object - not the file, but a means by which we access the file
 # always a good idea to tidy up
@@ -18,14 +19,43 @@ def writeToFile(text): # we ought to validate the incoming to make sure it is te
         chunk  = 24
         while True:
             if offset > size:
+                # we might choose to termitate our output with a new line character
+                fout.write('\n')
                 break
             else:
                 fout.write(text[offset:offset+chunk]) # [start:stop-before:step]
                 offset+=chunk
+    except FileExistsError as err:
+        print(f'The file already exists {err}')
     except Exception as err: # typically we have several exception handlers for different exceptions
         print(err)
     finally: # this will always run
         pass
 
+# read back
+def readFromFile():
+    '''declare a file access object to read back text'''
+    # we ought to use try-except block here...
+    fin = open('my_log.txt', 'r') # 'r' lets us read (text is the default)
+    received = fin.read() # just read the whole lot back
+    print(received)
+    fin.close()
+
+# seek within a text file
+def seekContent():
+    '''use file access objeect to seek content in a text file'''
+    # again, we ought to try-except
+    fin = open('my_log.txt', 'rt')
+    part = fin.readline()
+    print(f'first we read {part}')
+    # next we will seek just a few characters
+    fin.seek(24) # not common but sometimes useful
+    the_rest = fin.read() # from the seek point to the end
+    print(f'the rest is {the_rest}')
+    fin.close()
+
+
 if __name__ == '__main__':
     writeToFile('we can persist long strings of characters in a text file')
+    readFromFile()
+    seekContent()
