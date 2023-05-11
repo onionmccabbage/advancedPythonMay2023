e8e9f11f0c8dd26cf11a597df2f190bc2d927b82
onionmccabbage <onionmccabbage@live.com>
2023-05-11 10:38:16 +0100

    updated client

--- microservices/my_client.py
+++ microservices/my_client.py
@@ -6,7 +6,11 @@ def client():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # match the server
     port_t = ('localhost', 9874)
     sock.connect( port_t ) # we connect to the server
-    message='client says hello'
+    # check if any system arguments have been provided (remember sys.argv[0] is the module name)
+    if len(sys.argv) > 1:
+        message = ' '.join(sys.argv[1:]) # slice to ignore member 0
+    else:
+        message='client says hello'
     sock.send( message.encode() ) # we must always encode any string of data
     # we handle any response from the server
     data = sock.recv(1024) # read up to 1024 bytes
